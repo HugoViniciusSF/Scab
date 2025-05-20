4ebd56191952ecd2cfa2eee7df19ad53ef893e26
hugo <hugovenicius@hotmail.com.br>
2025-05-20 19:43:38 -0300

    Próximo do CriarSala1 só vai habilitar quando tiver quantidade de jogadores

--- scripts/CriarSala.gd
+++ scripts/CriarSala.gd
@@ -1,27 +1,38 @@
 extends Node2D
 
+@onready var proximo_botao = $ProximoBotao
+@onready var quantidade_input = $QuantidadeJogadoresLabel/QuantidadeJogadoresLine
+
 func _ready():
 	$VoltarBotao.pressed.connect(_on_botao_voltar_pressed)
-	$ProximoBotao.pressed.connect(_on_botao_proximo_pressed)
+	proximo_botao.pressed.connect(_on_botao_proximo_pressed)
+	quantidade_input.text_changed.connect(_on_quantidade_text_changed)
+
+	# Desativa botão inicialmente
+	proximo_botao.disabled = true
 
 func _on_botao_voltar_pressed():
 	get_tree().change_scene_to_file("res://scenes/Main.tscn")
 
 func _on_botao_proximo_pressed():
-	# Coletar as configurações dos CheckBoxes e LineEdit
 	var configuracoes = {
 		"revelar_funcoes": $GeralLabel/RevelarCheckBox.button_pressed,
 		"sem_mortes_noite": $GeralLabel/SemMortesCheckBox.button_pressed,
 		"habilitar_acoes": $GeralLabel/AcoesCheckBox.button_pressed,
-		
+
 		"pular_votacao": $VotacaoLabel/PularVotosCheckBox.button_pressed,
 		"esconder_votos": $VotacaoLabel/EsconderVotosCheckBox.button_pressed,
 
-		"quantidade_jogadores": int($QuantidadeJogadoresLabel/QuantidadeJogadoresLine.text)
+		"quantidade_jogadores": int(quantidade_input.text)
 	}
 
-	# Salvar no autoload (singleton)
 	JogoGlobal.configuracoes_sala = configuracoes
-
-	# Ir para próxima cena
 	get_tree().change_scene_to_file("res://scenes/Lobby.tscn")
+
+func _on_quantidade_text_changed(new_text):
+	# Ativa o botão apenas se for número válido maior que 0
+	var valor = new_text.strip_edges()
+	if valor.is_valid_int() and int(valor) > 0:
+		proximo_botao.disabled = false
+	else:
+		proximo_botao.disabled = true
